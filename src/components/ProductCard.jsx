5a140516b04217a7b134187fe7f6145845aa6772
sohanagashimani <sohanagashimanii@gmail.com>
2023-05-06 13:24:01 +0530

    Update: product card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -12,19 +12,25 @@ export default function ProductCard({ product }) {
   } = product;
   return (
     <div className="rounded-lg shadow-lg bg-white  text-black md:w-2/5 xl:w-1/4 flex flex-col justify-between">
-      <div className="p-4">
-        <h2 className="text-lg font-semibold">{name}</h2>
-        <Border />
-        <div className="flex justify-between mt-2">
-          <div className="text-gray-600">
-            Advertised rate: {advertisedRate}%
+      <div className="p-4 flex flex-col justify-between self-end flex-grow">
+        <div className="flex flex-col justify-between h-[9rem]">
+          <div>
+            <h2 className="text-lg font-semibold">{name}</h2>
           </div>
-          <div className="text-gray-600">
-            Comparison rate: {comparisonRate}%
+          <div>
+            <Border />
+            <div className="flex justify-between mt-2">
+              <div className="text-gray-600">
+                Advertised rate: {advertisedRate}%
+              </div>
+              <div className="text-gray-600">
+                Comparison rate: {comparisonRate}%
+              </div>
+            </div>
+            <Border />
           </div>
         </div>
-        <Border />
-        <div className="mt-2">
+        <div className="mt-2 flex-grow flex flex-col">
           <h3 className="text-md font-semibold mb-2">Pros:</h3>
           <ul className="list-disc pl-2">
             {pros.map((pro) => (
@@ -35,19 +41,21 @@ export default function ProductCard({ product }) {
             ))}
           </ul>
         </div>
-        <div className="mt-2">
+      </div>
+      <div className="flex flex-col justify-between">
+        <div className="px-4">
           <Border />
         </div>
-      </div>
-      <div className="p-4 flex justify-between items-center">
-        <img src={companyLogo} className="mr-auto w-24" alt={name} />
-        <div className="rounded-md  bg-green-500 p-1 px-2 text-white flex justify-between align-middle cursor-pointer">
-          <a href={gotoSiteUrl} target="_blank">
-            Go to site
-          </a>
-          <span className="mt-1 ml-2">
-            <BsArrowRightCircle />
-          </span>
+        <div className="p-4 flex justify-between items-center">
+          <img src={companyLogo} className="mr-auto w-24" alt={name} />
+          <div className="rounded-md  bg-green-500 p-1 px-2 text-white flex justify-between align-middle cursor-pointer">
+            <a href={gotoSiteUrl} target="_blank">
+              Go to site
+            </a>
+            <span className="mt-1 ml-2">
+              <BsArrowRightCircle />
+            </span>
+          </div>
         </div>
       </div>
     </div>
