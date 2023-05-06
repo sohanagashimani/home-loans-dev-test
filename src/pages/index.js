b4c987de4fe0745c1430b25dfd1d82700506cbc0
sohanagashimani <sohanagashimanii@gmail.com>
2023-05-06 13:11:49 +0530

    Fix: pagination

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,6 +34,7 @@ export default function Home() {
           }
         }
         setProducts(uniqueProducts);
+        setPageCount(data.meta.pageCount);
         setLoading(false);
       } catch (error) {
         console.log(error);
